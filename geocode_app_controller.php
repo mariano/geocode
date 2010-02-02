face79c00d8a2a4f87573f5a5997647e01d0c6e0
cdvrooman <enar@cheetah.webhostserver.biz>
2010-02-02 12:34:51 -0600

    Added plugin app_controller and app_model

---
+++ geocode_app_controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Geocode App Controller - nothing to see here
+ *
+ * @author Mariano Iglesias <*Coding Ninja* @ CRICAVA Technologies>
+ * @copyright 2010 (c) Mariano Iglesias
+ * @link http://cricava.com
+ * @package geocode
+ * @subpackage plugin
+ * @version $Id$
+ */
+class GeocodeAppController extends AppController {
+
+}
---
+++ geocode_app_model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Geocode App Model - nothing to see here
+ *
+ * @author Mariano Iglesias <*Coding Ninja* @ CRICAVA Technologies>
+ * @copyright 2010 (c) Mariano Iglesias
+ * @link http://cricava.com
+ * @package geocode
+ * @subpackage plugin
+ * @version $Id$
+ */
+class GeocodeAppModel extends AppModel {
+
+}
